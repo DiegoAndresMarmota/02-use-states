325fdb27552f8d4099c46cda3300a5622e6f7ee1
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-07-06 12:58:54 -0400

    Actualizacion

---
+++ src/components/MiPrimerEstado.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+export const MiPrimerEstado = () => {
+  
+    /*
+    //Problematica
+    let nombre = "Diego Andres";
+
+    const cambiarNombre = e => {
+        nombre = "Max Power";
+    };
+    */
+    
+    const [nombre, setNombre] = useState("Diego Andrés");
+
+    const cambiarNombre = e => {
+        setNombre("Max Power");
+    }
+  
+    return (
+    <div>
+        <h3>Componente: MiPrimerEstado</h3>
+        <strong>
+            {nombre}        
+            </strong>
+            &nbsp;
+            <button onClick={ cambiarNombre }>
+            Cambiar
+            </button>
+    </div>
+  )
+}
